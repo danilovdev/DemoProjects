2c209849df3f3874c72b2f090faa1b6284fe6c49
danilovdev <danilovdev@gmail.com>
2021-10-09 11:26:00 +0300

    init ios job interview practice project

---
+++ JobInterviewPractice/Shared/JobInterviewPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobInterviewPracticeApp.swift
+//  Shared
+//
+//  Created by Aleksei Danilov on 09.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct JobInterviewPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
